4b8b54ddb34af3b8034e37c1d7968c9fb3e98ffd
Pascal <pas111cal3@gmail.com>
2026-06-08 01:24:56 +0100

    made user role default to associated instead of user , in schema

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
     role: {
     type: String,
     enum: ["admin", "associate"],
-    default: "user",
+    default: "associate",
   },
      rank: {
     type: String,
